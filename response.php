94be1a7eab4111c69789c3b400f96081f33ba71d
TALEX1995 <tanase.alex@libero.it>
2023-07-17 16:04:35 +0200

    Get form input name

---
+++ response.php
@@ -0,0 +1,22 @@
+<?php
+
+$name = $_GET[trim('name')];
+$e_mail = $_GET[trim('e-mail')];
+$age = $_GET['age'];
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
